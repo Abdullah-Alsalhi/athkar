88251a9baad4810cb609d02d842bca94cc13c21c
Abdullah-Alsalhi <abuhamed19@gmail.com>
2022-06-14 15:12:43 +0300

    main page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
+    <div className="App page">
+      <Navbar/>
+      <div className="container" >
       <Header/>
-      <div className="d-grid gap-2 col-6 mx-auto day-thekr">
-        <button className="btn btn-primary" type="button">أذكار الصباح</button>
-        <button className="btn btn-primary" type="button">أذكار المساء</button>
+      <div className="d-grid gap-2 col-6 mx-auto mt-4">
+        <button className="btn btn-success" type="button">أذكار الصباح</button>
+        <button className="btn btn-success" type="button">أذكار المساء</button>
       </div>
-      
       <Footer/>
+      </div>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="footer alert alert-dark">
-      <h1>هذا العمل صدقة عن المسلمين والمسلمات اﻷحياء منهم واﻷموات</h1>
+    <div className="footer">
+      
     </div>
   );
 }
