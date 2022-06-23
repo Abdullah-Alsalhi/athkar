2811dfdde41519a9e6b6c392846db0234a25373d
Abdullah-Alsalhi <abuhamed19@gmail.com>
2022-06-23 19:38:48 +0300

    make the button countable

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='mt-5 text-center Header'>
-      <h1>فاذكروني أذكركم</h1>
+      <h1>ألا بذكر الله تطمئن القلوب</h1>
     </div>
   )
 }
--- src/components/Morning.js
+++ src/components/Morning.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 import athkar from '../thekr-json/athkar.json'
 export default function Morning() {
-  const [counter, setCounter] = useState(0)
-  console.log(counter)
+  
   return (
     <div className=''>
       {athkar.map(e => { return (
@@ -14,7 +13,13 @@ export default function Morning() {
             <h5 className='card-title'>{e.category}</h5>
             <p className='card-text'>{e.zekr}</p>
             <div className='d-grid gap-2 col-6 mx-auto mt-4'>
-            <button type='button' className='btn btn-success' >{e.count}</button>
+            <button type='button' className='btn btn-success' onClick={(event)=> {
+              if (event.target.innerText == 1) {
+                event.target.style = "background: blue";
+                event.target.disabled = true;
+              }
+              event.target.innerText--;
+            }}>{e.count}</button>
             </div>
           </div>
           <div class="card-footer text-muted">
