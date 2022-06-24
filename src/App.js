eb0c79031e78637dcfaabcc460893e60a0f1ea02
Abdullah-Alsalhi <abuhamed19@gmail.com>
2022-06-24 15:50:19 +0300

    filtering athkar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Morning from "./components/Morning";
 import Hadith from "./components/Hadith";
+import Evining from "./components/Evining";
 function App() {
   return (
     <div className="App page">
@@ -44,6 +45,16 @@ function App() {
         </Routes>
         {/* end of morning athkar page */}
 
+        {/* this route will take you to evining athkar page */}
+        <Routes>
+          <Route path="/massa" element={
+          <div className="d-grid gap-2 col-6 mx-auto mt-4">
+          <Evining />
+          <Link to="/" className="btn btn-success" type="button">إلى الصفحة الرئيسية</Link>
+          </div>
+          } />
+        </Routes>
+        {/* end of evining athkar page */}
         {/* missing_page route */}
         <Routes>
           <Route path="/missing_page" element={<h1 >لم يتم العمل على هذه الصفحة حتى الان</h1>}/>
--- src/components/Evining.js
+++ src/components/Evining.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import athkar from '../thekr-json/athkar.json'
 
 export default function Evining() {
   return (
-    <div>Evining</div>
+    <div className=''>
+      {athkar.filter(e => e.category === "أذكار المساء").map(e => { return (
+        <div className='card text-center my-3'>
+          <div className='card-header'>
+            {e.reference}
+          </div>
+          <div className='card-body'>
+            <h5 className='card-title'>{e.category}</h5>
+            <p className='card-text'>{e.zekr}</p>
+            <div className='d-grid gap-2 col-6 mx-auto mt-4'>
+            <button type='button' className='btn btn-success' onClick={(event)=> {
+
+              if (event.target.innerText == "انتقل للذكر التالي") {
+                
+                event.target.disabled = true;
+                return;
+              }
+
+              if (event.target.innerText == 0) {
+                event.target.style = "background: #0a4c59";
+                event.currentTarget.innerText = "انتقل للذكر التالي";
+                return;
+              }
+              if (event.target.innerText == 1) {
+                event.target.style = "background: #0a4c59";
+                
+              }
+              event.target.innerText--;
+            }}>{e.count}</button>
+            </div>
+          </div>
+          <div class="card-footer text-muted">
+              تجربة
+          </div>
+        </div>
+      )})}
+    </div>
   )
 }
