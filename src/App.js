4b6006b1c1e34ccd8f7668f19cef0061897b4dde
Abdullah-Alsalhi <abuhamed19@gmail.com>
2022-07-03 01:22:46 +0300

    layout design

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
         {/* this route will take you to moring athkar page */}
         <Routes>
           <Route path="/sabah" element={
-          <div className="d-grid gap-2 col-6 mx-auto mt-4">
+          <div >
           <Morning />
           <Link to="/" className="btn btn-success" type="button">إلى الصفحة الرئيسية</Link>
           </div>
@@ -48,7 +48,7 @@ function App() {
         {/* this route will take you to evining athkar page */}
         <Routes>
           <Route path="/massa" element={
-          <div className="d-grid gap-2 col-6 mx-auto mt-4">
+          <div >
           <Evining />
           <Link to="/" className="btn btn-success" type="button">إلى الصفحة الرئيسية</Link>
           </div>
--- src/components/Evining.js
+++ src/components/Evining.js
@@ -3,42 +3,47 @@ import athkar from '../thekr-json/athkar.json'
 
 export default function Evining() {
   return (
-    <div className=''>
-      {athkar.filter(e => e.category === "أذكار المساء").map(e => { return (
-        <div className='card text-center my-3'>
-          <div className='card-header'>
-            {e.reference}
-          </div>
-          <div className='card-body'>
-            <h5 className='card-title'>{e.category}</h5>
-            <p className='card-text'>{e.zekr}</p>
-            <div className='d-grid gap-2 col-6 mx-auto mt-4'>
-            <button type='button' className='btn btn-success' onClick={(event)=> {
+    <div className='text-center'>
+      
+            <div className='row'>
+            <div className='col-md-12'>
 
-              if (event.target.innerText == "انتقل للذكر التالي") {
+            {athkar.filter(e => e.category === "أذكار الصباح").map(e => {
+              return (
+                <div className='my-3 shadow my-4'>
+                      <h6 className=''>
+                        {e.reference}
+                      </h6>
+                      <p className=''>
+                        {e.zekr}
+                      </p>
+                      <p className=''>
+                        {e.description}
+                      </p>
+                      <button className='btn btn-success col-md-5 my-3' onClick={(event)=>{
+                        if (event.target.innerText == "انتقل للذكر التالي") {
                 
-                event.target.disabled = true;
-                return;
-              }
-
-              if (event.target.innerText == 0) {
-                event.target.style = "background: #0a4c59";
-                event.currentTarget.innerText = "انتقل للذكر التالي";
-                return;
-              }
-              if (event.target.innerText == 1) {
-                event.target.style = "background: #0a4c59";
-              
-              }
-              event.target.innerText--;
-            }}>{e.count}</button>
+                          event.target.disabled = true;
+                          return;
+                        }
+          
+                        if (event.target.innerText == 0) {
+                          event.target.style = "background: #0a4c59";
+                          event.currentTarget.innerText = "انتقل للذكر التالي";
+                          return;
+                        }
+                        if (event.target.innerText == 1) {
+                          event.target.style = "background: #0a4c59";
+                        
+                        }
+                        event.target.innerText--;
+                      }}>{e.count}</button>
+                </div>
+              )
+            })}
+            </div>
             </div>
           </div>
-          <div class="card-footer text-muted">
-              تجربة
-          </div>
-        </div>
-      )})}
-    </div>
+
   )
 }
